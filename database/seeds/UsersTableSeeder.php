d17bc641a4d6ba6154743c7c3bca60764e12b2c4
吴彦文 <1316327330@qq.com>
2018-03-12 17:16:04 +0800

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,14 +12,14 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $this->insertUser();
-        $this->insertPermissions();
+        $this->insertGetIdUser();
+        $this->insertGetIdPermissions();
     }
     
     
-    protected function insertUser()
+    protected function insertGetIdUser()
     {
-        DB::table('users')->insert([
+        DB::table('users')->insertGetId([
             'name' => 'admin',
             'email' => 'admin@gmail.com',
             'password' => bcrypt('admin'),
@@ -28,9 +28,9 @@ class UsersTableSeeder extends Seeder
         ]);
     }
     
-    protected function insertPermissions()
+    protected function insertGetIdPermissions()
     {
-        $ppid = DB::table('permissions')->insert([
+        $ppid = DB::table('permissions')->insertGetId([
             'name' => '权限管理',
             'pid'  => 0,
             'url' => '#',
@@ -38,91 +38,91 @@ class UsersTableSeeder extends Seeder
             'weight' => 10,
         ]);
         
-        $upid = DB::table('permissions')->insert([
+        $upid = DB::table('permissions')->insertGetId([
             'name' => '用户管理',
             'pid'  => $ppid,
             'url' => 'user',
-            'behavior' => 'user@index',
+            'behavior' => 'users@index',
             'weight' => 10,
         ]);
-         DB::table('permissions')->insert([
+         DB::table('permissions')->insertGetId([
             'name' => '添加用户',
             'pid'  => $upid,
             'url' => 'user/create',
-            'behavior' => 'user@create',
+            'behavior' => 'users@create',
             'weight' => 1,
         ]);
-        DB::table('permissions')->insert([
+        DB::table('permissions')->insertGetId([
             'name' => '编辑用户',
             'pid'  => $upid,
             'url' => 'user/edit',
-            'behavior' => 'user@edit',
+            'behavior' => 'users@edit',
             'weight' => 1,
         ]);
-         DB::table('permissions')->insert([
+         DB::table('permissions')->insertGetId([
             'name' => '删除用户',
             'pid'  => $upid,
             'url' => 'user/destory',
-            'behavior' => 'user@destory',
+            'behavior' => 'users@destory',
             'weight' => 1,
         ]);
          
-         $rpid = DB::table('permissions')->insert([
+         $rpid = DB::table('permissions')->insertGetId([
              'name' => '角色管理',
              'pid'  => $ppid,
              'url' => 'role',
-             'behavior' => 'role@index',
+             'behavior' => 'roles@index',
              'weight' => 9,
          ]);
-         DB::table('permissions')->insert([
+         DB::table('permissions')->insertGetId([
              'name' => '添加角色',
              'pid'  => $rpid,
              'url' => 'role/create',
-             'behavior' => 'role@create',
+             'behavior' => 'roles@create',
              'weight' => 1,
          ]);
-         DB::table('permissions')->insert([
+         DB::table('permissions')->insertGetId([
              'name' => '编辑角色',
              'pid'  => $rpid,
              'url' => 'role/edit',
-             'behavior' => 'role@edit',
+             'behavior' => 'roles@edit',
              'weight' => 1,
          ]);
-         DB::table('permissions')->insert([
+         DB::table('permissions')->insertGetId([
              'name' => '删除角色',
              'pid'  => $rpid,
              'url' => 'role/destory',
-             'behavior' => 'role@destory',
+             'behavior' => 'roles@destory',
              'weight' => 1,
          ]);
          
          
-         $ppid = DB::table('permissions')->insert([
+         $ppid = DB::table('permissions')->insertGetId([
              'name' => '菜单管理',
              'pid'  => $ppid,
              'url' => 'permission',
-             'behavior' => 'permission@index',
+             'behavior' => 'permissions@index',
              'weight' => 10,
          ]);
-         DB::table('permissions')->insert([
+         DB::table('permissions')->insertGetId([
              'name' => '添加菜单',
              'pid'  => $ppid,
              'url' => 'permission/create',
-             'behavior' => 'permission@create',
+             'behavior' => 'permissions@create',
              'weight' => 1,
          ]);
-         DB::table('permissions')->insert([
+         DB::table('permissions')->insertGetId([
              'name' => '编辑菜单',
              'pid'  => $ppid,
              'url' => 'permission/edit',
-             'behavior' => 'permission@edit',
+             'behavior' => 'permissions@edit',
              'weight' => 1,
          ]);
-         DB::table('permissions')->insert([
+         DB::table('permissions')->insertGetId([
              'name' => '删除菜单',
              'pid'  => $ppid,
              'url' => 'permission/destory',
-             'behavior' => 'permission@destory',
+             'behavior' => 'permissions@destory',
              'weight' => 1,
          ]);
     }
