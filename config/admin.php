77d7303a7c4ceee532c3734cfe78b3e8a0476c1a
吴彦文 <1316327330@qq.com>
2018-04-06 16:42:49 +0800

    Update admin.php

--- config/admin.php
+++ config/admin.php
@@ -1,8 +1,19 @@
 <?php
 
 return [
-    
-
+    'auth' => [
+        'guards' => [
+            'admin' => [
+                'driver' => 'session',
+                'provider' => 'admin_users',
+            ],
+        ],
+        
+        'admin_users'  => [
+            'driver' => 'eloquent',
+            'model' => Lizyu\Admin\Model\User::class,
+        ],
+    ],
     
     'table' => 'admin_users',
     
